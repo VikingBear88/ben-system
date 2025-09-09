3d0821d378b494c20e76108262ea332156bb0de5
VikingBear88 <benranson@hotmail.co.uk>
2025-09-09 12:39:25 +0100

    Add skill-perception-sheet.mjs

---
+++ module/sheets/skill-perception-sheet.mjs
@@ -0,0 +1,26 @@
+import { AbilityBaseSheet } from "./ability-base-sheet.mjs";
+
+export class SkillPerceptionSheet extends AbilityBaseSheet {
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      classes: ["ben-system", "sheet", "item"],
+      width: 640,
+      height: 580,
+      tabs: [
+        { navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "details" }
+      ]
+    });
+  }
+
+  get template() {
+    return "systems/ben-system/templates/item/item-skill-generic-sheet.hbs";
+  }
+
+  async getData(options) {
+    const ctx = await super.getData(options);
+    ctx.item = this.item;
+    ctx.system = this.item.system ?? {};
+    ctx.editable = this.isEditable;
+    return ctx;
+  }
+}
